646dad989986baba5b3ddf8e836456f88bc69d1d
Mikail Khan <mikail.khan45@gmail.com>
2021-02-15 23:41:05 -0500

    updated example

--- src/main.rs
+++ src/main.rs
@@ -81,27 +81,31 @@ fn main() {
         .watch(filename, notify::RecursiveMode::NonRecursive)
         .unwrap();
 
+    let rebuild = || {
+        println!("rebuilding pdf");
+        let contents = std::fs::read_to_string(filename).unwrap();
+
+        let mut state = State::new(&contents);
+
+        state.exec();
+
+        let mut md_file = tempfile::NamedTempFile::new().unwrap();
+        write!(md_file, "{}", state.output).unwrap();
+
+        let mut pandoc = pandoc::new();
+        pandoc.set_input_format(pandoc::InputFormat::Latex, Vec::new());
+        pandoc.add_input(&md_file.path());
+
+        pandoc.set_output(pandoc::OutputKind::File(args[2].to_string().into()));
+        pandoc.execute().unwrap();
+        println!("done rebuilding pdf");
+    };
+
+    rebuild();
+
     loop {
         match rx.recv() {
-            Ok(_) => {
-                println!("rebuilding pdf");
-                let contents = std::fs::read_to_string(filename).unwrap();
-
-                let mut state = State::new(&contents);
-
-                state.exec();
-
-                let mut md_file = tempfile::NamedTempFile::new().unwrap();
-                write!(md_file, "{}", state.output).unwrap();
-
-                let mut pandoc = pandoc::new();
-                pandoc.set_input_format(pandoc::InputFormat::Latex, Vec::new());
-                pandoc.add_input(&md_file.path());
-
-                pandoc.set_output(pandoc::OutputKind::File(args[2].to_string().into()));
-                pandoc.execute().unwrap();
-                println!("done rebuilding pdf");
-            }
+            Ok(_) => rebuild(),
             Err(e) => println!("watch error: {:?}", e),
         }
     }
