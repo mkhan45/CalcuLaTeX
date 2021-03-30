0fd6540fcdaf6b69b0f185dcb0d09261e404c5ea
Mikail Khan <mikail.khan45@gmail.com>
2021-03-30 12:12:04 -0400

    file tests
    part of #8

--- src/lib.rs
+++ src/lib.rs
@@ -15,3 +15,37 @@ pub fn generate_latex(input: &str) -> Result<String, CalcError> {
     state.exec()?;
     Ok(state.output)
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::CalcError;
+
+    fn run_on_file(filename: &str) -> Result<String, CalcError> {
+        let contents = std::fs::read_to_string(filename).unwrap();
+        super::generate_latex(&contents)
+    }
+
+    macro_rules! test_file {
+        ( $name: ident ) => {
+            #[test]
+            fn $name() {
+                let input_name = format!("test_files/{}.math", stringify!($name));
+                let output_name = format!("test_outputs/{}.tex", stringify!($name));
+
+                let output = run_on_file(input_name.as_str()).unwrap().replace(" ", "");
+                println!("{}", output);
+                assert_eq!(
+                    output.trim(),
+                    std::fs::read_to_string(output_name.as_str())
+                        .unwrap()
+                        .trim()
+                        .replace(" ", ""),
+                );
+            }
+        };
+    }
+
+    test_file!(basic);
+    test_file!(function);
+    test_file!(tutorial);
+}
